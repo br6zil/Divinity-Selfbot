c247ec59a8f25562056fe10e790c64166759221a
leo <85583839+01Leozin@users.noreply.github.com>
2021-08-22 18:24:14 -0300

    commands.py

---
+++ scripts/commands.py
@@ -0,0 +1,428 @@
+@divinity.command()
+async def uptime(ctx):
+    await ctx.message.delete()
+    now = datetime.utcnow()
+    delta = now - start_time
+    hours, remainder = divmod(int(delta.total_seconds()), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days, hours = divmod(hours, 24)
+    if days:
+        time_format = "{d} days, {h} hours, {m} minutes, and {s} seconds."
+    else:
+        time_format = "{h} hours, {m} minutes, and {s} seconds."
+    uptime_stamp = time_format.format(d=days, h=hours, m=minutes, s=seconds)
+    await ctx.send(uptime_stamp)
+
+@divinity.command()
+async def unflip(ctx):
+    await ctx.message.delete()
+    unflip = '┬─┬ ノ( ゜-゜ノ)'
+    await ctx.send(unflip)
+
+@divinity.command()
+async def tts(ctx, *, message):
+    await ctx.message.delete()
+    buff = await do_tts(message)
+    await ctx.send(file=discord.File(buff, f"{message}.wav"))
+
+@divinity.command()
+async def tokeninfo(ctx, _token):
+    await ctx.message.delete()
+    headers = {
+        'Authorization': _token,
+        'Content-Type': 'application/json'
+    }
+    try:
+        res = requests.get('https://canary.discordapp.com/api/v6/users/@me', headers=headers)
+        res = res.json()
+        user_id = res['id']
+        locale = res['locale']
+        avatar_id = res['avatar']
+        language = languages.get(locale)
+        creation_date = datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime(
+            '%d-%m-%Y %H:%M:%S UTC')
+    except KeyError:
+        headers = {
+            'Authorization': "Bot " + _token,
+            'Content-Type': 'application/json'
+        }
+        try:
+            res = requests.get('https://canary.discordapp.com/api/v6/users/@me', headers=headers)
+            res = res.json()
+            user_id = res['id']
+            locale = res['locale']
+            avatar_id = res['avatar']
+            language = languages.get(locale)
+            creation_date = datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime(
+                '%d-%m-%Y %H:%M:%S UTC')
+            em = discord.Embed(
+                description=f"Name: `{res['username']}#{res['discriminator']} ` **BOT**\nID: `{res['id']}`\nEmail: `{res['email']}`\nCreation Date: `{creation_date}`")
+            fields = [
+                {'name': 'Flags', 'value': res['flags']},
+                {'name': 'Local language', 'value': res['locale'] + f"{language}"},
+                {'name': 'Verified', 'value': res['verified']},
+            ]
+            for field in fields:
+                if field['value']:
+                    em.add_field(name=field['name'], value=field['value'], inline=False)
+                    em.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}")
+            return await ctx.send(embed=em)
+        except KeyError:
+            await ctx.send("Invalid token")
+    em = discord.Embed(
+        description=f"Name: `{res['username']}#{res['discriminator']}`\nID: `{res['id']}`\nEmail: `{res['email']}`\nCreation Date: `{creation_date}`")
+    em.set_footer(text=f"/divinity")
+    nitro_type = "None"
+    if "premium_type" in res:
+        if res['premium_type'] == 2:
+            nitro_type = "Nitro"
+        elif res['premium_type'] == 1:
+            nitro_type = "Nitro Classic"
+    fields = [
+        {'name': 'Phone', 'value': res['phone']},
+        {'name': 'Flags', 'value': res['flags']},
+        {'name': 'Local language', 'value': res['locale'] + f"{language}"},
+        {'name': 'MFA', 'value': res['mfa_enabled']},
+        {'name': 'Verified', 'value': res['verified']},
+        {'name': 'Nitro', 'value': nitro_type},
+    ]
+    for field in fields:
+        if field['value']:
+            em.add_field(name=field['name'], value=field['value'], inline=False)
+            em.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}")
+    return await ctx.send(embed=em)
+
+@divinity.command(aliases=["stopautoname", "stoploopname", "sautoname", "sloopname", "slname"])
+async def stoplname(ctx):
+    await ctx.message.delete()
+    global lname
+    lname = False
+
+@divinity.command()
+async def shrug(ctx):
+    await ctx.message.delete()
+    shrug = r'¯\_(ツ)_/¯'
+    await ctx.send(shrug)
+
+@divinity.command(aliases=['svico', 'guildicon'])
+async def servericon(ctx):
+    await ctx.message.delete()
+    em = discord.Embed(title=f"{ctx.guild.name} - Divinity Selfbot")
+    em.set_image(url=ctx.guild.icon_url)
+    await ctx.send(embed=em)
+
+@divinity.command()
+async def reverse(ctx, *, message):
+    await ctx.message.delete()
+    message = message[::-1]
+    await ctx.send(message)
+
+@divinity.command()
+async def lenny(ctx):
+    await ctx.message.delete()
+    lenny = '( ͡° ͜ʖ ͡°)'
+    await ctx.send(lenny)
+
+@divinity.command()
+async def hentai(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/Random_hentai_gif")
+    res = r.json()
+    try:
+        async with aiohttp.divinitySession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"divinity_hentai.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+@divinity.command(aliases=["autoname", "loopname"])
+async def lname(ctx, *, text):
+    await ctx.message.delete()
+    global lname
+    lname = True
+    while lname:
+        name = ""
+        for letter in text:
+            name = name + letter
+            await ctx.message.author.edit(nick=name)
+
+@divinity.command()
+async def magik(ctx, user: discord.Member = None):
+    await ctx.message.delete()
+    endpoint = "https://nekobot.xyz/api/imagegen?type=magik&intensity=3&image="
+    if user is None:
+        avatar = str(ctx.author.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_magik.png"))
+        except:
+            await ctx.send(res['message'])
+    else:
+        avatar = str(user.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_magik.png"))
+        except:
+            await ctx.send(res['message'])
+
+@divinity.command()
+async def massreact(ctx, emote):
+    await ctx.message.delete()
+    messages = await ctx.message.channel.history(limit=20).flatten()
+    for message in messages:
+        await message.add_reaction(emote)
+
+@divinity.command(aliases=["pornhubcomment", "phc"])
+async def phcomment(ctx, user: str = None, *, args=None):
+    await ctx.message.delete()
+    if user is None or args is None:
+        await ctx.send("missing parameters")
+        return
+    endpoint = "https://nekobot.xyz/api/imagegen?type=phcomment&text=" + args + "&username=" + user + "&image=" + str(
+        ctx.author.avatar_url_as(format="png"))
+    r = requests.get(endpoint)
+    res = r.json()
+    try:
+        async with aiohttp.divinitySession() as session:
+            async with session.get(res["message"]) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"divinity_pornhub_comment.png"))
+    except:
+        await ctx.send(res["message"])
+
+@divinity.command(name='first-message', aliases=['firstmsg', 'fm', 'firstmessage'])
+async def _first_message(ctx, channel: discord.TextChannel = None):
+    await ctx.message.delete()
+    if channel is None:
+        channel = ctx.channel
+    first_message = (await channel.history(limit=1, oldest_first=True).flatten())[0]
+    embed = discord.Embed(description=first_message.content)
+    embed.add_field(name="First Message", value=f"[Jump]({first_message.jump_url})")
+    await ctx.send(embed=embed)
+
+@divinity.command()
+async def fliptable(ctx):
+    await ctx.message.delete()
+    tableflip = '(╯°□°）╯︵ ┻━┻'
+    await ctx.send(tableflip)
+
+@divinity.command()
+async def fry(ctx, user: discord.Member = None):
+    await ctx.message.delete()
+    endpoint = "https://nekobot.xyz/api/imagegen?type=deepfry&image="
+    if user is None:
+        avatar = str(ctx.author.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_fry.png"))
+        except:
+            await ctx.send(res['message'])
+    else:
+        avatar = str(user.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_fry.png"))
+        except:
+            await ctx.send(res['message'])
+
+@divinity.command()
+async def anal(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/anal")
+    res = r.json()
+    try:
+        async with aiohttp.divinitySession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"divinity_anal.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+@divinity.command(aliases=['pfp', 'avatar'])
+async def av(ctx, *, user: discord.Member = None):
+    await ctx.message.delete()
+    format = "gif"
+    user = user or ctx.author
+    if user.is_avatar_animated() != True:
+        format = "png"
+    avatar = user.avatar_url_as(format=format if format != "gif" else None)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(str(avatar)) as resp:
+            image = await resp.read()
+    with io.BytesIO(image) as file:
+        await ctx.send(file=discord.File(file, f"Avatar.{format}"))
+
+@divinity.command()
+async def blurpify(ctx, user: discord.Member = None):
+    await ctx.message.delete()
+    endpoint = "https://nekobot.xyz/api/imagegen?type=blurpify&image="
+    if user is None:
+        avatar = str(ctx.author.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_blurpify.png"))
+        except:
+            await ctx.send(res['message'])
+    else:
+        avatar = str(user.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_blurpify.png"))
+        except:
+            await ctx.send(res['message'])
+
+@divinity.command(aliases=['ejacular'])
+async def cum(ctx):
+    await ctx.message.delete()
+    message = await ctx.send('''
+            :ok_hand:            :smile:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8=:punch:=D 
+             :trumpet:      :eggplant:''')
+    await asyncio.sleep(0.5)
+    await message.edit(content='''
+                      :ok_hand:            :smiley:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8==:punch:D 
+             :trumpet:      :eggplant:  
+     ''')
+    await asyncio.sleep(0.5)
+    await message.edit(content='''
+                      :ok_hand:            :grimacing:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8=:punch:=D 
+             :trumpet:      :eggplant:  
+     ''')
+    await asyncio.sleep(0.5)
+    await message.edit(content='''
+                      :ok_hand:            :persevere:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8==:punch:D 
+             :trumpet:      :eggplant:   
+     ''')
+    await asyncio.sleep(0.5)
+    await message.edit(content='''
+                      :ok_hand:            :confounded:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8=:punch:=D 
+             :trumpet:      :eggplant: 
+     ''')
+    await asyncio.sleep(0.5)
+    await message.edit(content='''
+                       :ok_hand:            :tired_face:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8==:punch:D 
+             :trumpet:      :eggplant:    
+             ''')
+    await asyncio.sleep(0.5)
+    await message.edit(contnet='''
+                       :ok_hand:            :weary:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8=:punch:= D:sweat_drops:
+             :trumpet:      :eggplant:        
+     ''')
+    await asyncio.sleep(0.5)
+    await message.edit(content='''
+                       :ok_hand:            :dizzy_face:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8==:punch:D :sweat_drops:
+             :trumpet:      :eggplant:                 :sweat_drops:
+     ''')
+    await asyncio.sleep(0.5)
+    await message.edit(content='''
+                       :ok_hand:            :drooling_face:
+   :eggplant: :zzz: :necktie: :eggplant: 
+                   :oil:     :nose:
+                 :zap: 8==:punch:D :sweat_drops:
+             :trumpet:      :eggplant:                 :sweat_drops:
+     ''')
+
+@divinity.command()
+async def erofeet(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/erofeet")
+    res = r.json()
+    try:
+        async with aiohttp.divinitySession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"divinity_erofeet.png"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+@divinity.command()
+async def empty(ctx):
+    await ctx.message.delete()
+    await ctx.send(chr(173))
+
+@divinity.command()
+async def feet(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/feetg")
+    res = r.json()
+    try:
+        async with aiohttp.divinitySession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"divinity_feet.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
