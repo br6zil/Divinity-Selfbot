da532ee633402049e0fe78198e3a3323f4cdb5cc
leo <85583839+01Leozin@users.noreply.github.com>
2021-08-21 22:33:21 -0300

    Uptime Command

--- commands/uptime.py
+++ commands/uptime.py
@@ -1,8 +1,8 @@
 import base64, codecs
-magic = 'QGRpdmluaXR5LmNvbW1hbmQoKQ0KYXN5bmMgZGVmIHVwdGltZShjdHgpOg0KICAgIGF3YWl0IGN0eC5tZXNzYWdlLmRlbGV0ZSgpDQogICAgbm93ID0gZGF0ZXRpbWUudXRjbm93KCkNCiAgICBkZWx0YSA9IG5vdyAtIHN0YXJ0X3RpbWUNCiAgICB'
-love = 'bo3IlpljtpzIgLJyhMTIlVQ0tMTy2oJ9xXTyhqPuxMJk0LF50o3EuoS9mMJAiozEmXPxcYPNmAwNjXD0XVPNtVT1coaI0MKZfVUAyL29hMUZtCFOxnKMgo2DbpzIgLJyhMTIlYPN2ZPxAPvNtVPOxLKymYPObo3IlplN9VTEcqz1iMPubo3IlpljtZw'
-god = 'QpDQogICAgaWYgZGF5czoNCiAgICAgICAgdGltZV9mb3JtYXQgPSAie2R9IGRheXMsIHtofSBob3Vycywge219IG1pbnV0ZXMsIGFuZCB7c30gc2Vjb25kcy4iDQogICAgZWxzZToNCiAgICAgICAgdGltZV9mb3JtYXQgPSAie2h9IGhvdXJzLCB7b'
-destiny = 'K0toJyhqKEypljtLJ5xVUgmsFOmMJAiozEmYvVAPvNtVPO1pUEcoJIsp3EuoKNtCFO0nJ1yK2Mipz1uqP5zo3WgLKDbMQ1xLKymYPObCJuiqKWmYPOgCJ1coaI0MKZfVUZ9p2Iwo25xplxAPvNtVPOuq2ScqPOwqUthp2IhMPu1pUEcoJIsp3EuoKNc'
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+uptime = 'QGRpdmluaXR5LmNvbW1hbmQoKQ0KYXN5bmMgZGVmIHVwdGltZShjdHgpOg0KICAgIGF3YWl0IGN0eC5tZXNzYWdlLmRlbGV0ZSgpDQogICAgbm93ID0gZGF0ZXRpbWUudXRjbm93KCkNCiAgICBkZWx0YSA9IG5vdyAtIHN0YXJ0X3RpbWUNCiAgICB'
+command = 'bo3IlpljtpzIgLJyhMTIlVQ0tMTy2oJ9xXTyhqPuxMJk0LF50o3EuoS9mMJAiozEmXPxcYPNmAwNjXD0XVPNtVT1coaI0MKZfVUAyL29hMUZtCFOxnKMgo2DbpzIgLJyhMTIlYPN2ZPxAPvNtVPOxLKymYPObo3IlplN9VTEcqz1iMPubo3IlpljtZw'
+divinity = 'QpDQogICAgaWYgZGF5czoNCiAgICAgICAgdGltZV9mb3JtYXQgPSAie2R9IGRheXMsIHtofSBob3Vycywge219IG1pbnV0ZXMsIGFuZCB7c30gc2Vjb25kcy4iDQogICAgZWxzZToNCiAgICAgICAgdGltZV9mb3JtYXQgPSAie2h9IGhvdXJzLCB7b'
+selfbot = 'K0toJyhqKEypljtLJ5xVUgmsFOmMJAiozEmYvVAPvNtVPO1pUEcoJIsp3EuoKNtCFO0nJ1yK2Mipz1uqP5zo3WgLKDbMQ1xLKymYPObCJuiqKWmYPOgCJ1coaI0MKZfVUZ9p2Iwo25xplxAPvNtVPOuq2ScqPOwqUthp2IhMPu1pUEcoJIsp3EuoKNc'
+leozin = '\x72\x6f\x74\x31\x33'
+lol = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
 eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
