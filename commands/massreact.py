299d4ac3c586df4b1a3898414e991d85f8ac05b5
leo <85583839+01Leozin@users.noreply.github.com>
2021-08-21 23:10:19 -0300

    Create massreact.py

---
+++ commands/massreact.py
@@ -0,0 +1,6 @@
+@divinity.command()
+async def massreact(ctx, emote):
+    await ctx.message.delete()
+    messages = await ctx.message.channel.history(limit=20).flatten()
+    for message in messages:
+        await message.add_reaction(emote)
