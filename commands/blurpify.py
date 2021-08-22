71f43f85ba349102c4d9304b2418b8eba3ac695a
leo <85583839+01Leozin@users.noreply.github.com>
2021-08-22 18:12:57 -0300

    Create blurpify.py

---
+++ commands/blurpify.py
@@ -0,0 +1,30 @@
+@divinity.command()
+async def blurpify(ctx, user: discord.Member = None):
+    await ctx.message.delete()
+    endpoint = "https://nekobot.xyz/api/imagegen?type=blurpify&image="
+    if user is None:
+        avatar = str(ctx.author.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_blurpify.png"))
+        except:
+            await ctx.send(res['message'])
+    else:
+        avatar = str(user.avatar_url_as(format="png"))
+        endpoint += avatar
+        r = requests.get(endpoint)
+        res = r.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(str(res['message'])) as resp:
+                    image = await resp.read()
+            with io.BytesIO(image) as file:
+                await ctx.send(file=discord.File(file, f"divinity_blurpify.png"))
+        except:
+            await ctx.send(res['message'])
