cf74b04a8f4d00b07e4f3c4d1d455db0bdb068a8
leo <85583839+01Leozin@users.noreply.github.com>
2021-08-21 22:53:36 -0300

    Tokeninfo Command

---
+++ commands/tokeninfo.py
@@ -0,0 +1,66 @@
+@divinity.command()
+async def tokeninfo(ctx, _token):
+    await ctx.message.delete()
+    headers = {
+        'Authorization': _token,
+        'Content-Type': 'application/json'
+    }
+    try:
+        res = requests.get('https://canary.discordapp.com/api/v6/users/@me', headers=headers)
+        res = res.json()
+        user_id = res['id']
+        locale = res['locale']
+        avatar_id = res['avatar']
+        language = languages.get(locale)
+        creation_date = datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime(
+            '%d-%m-%Y %H:%M:%S UTC')
+    except KeyError:
+        headers = {
+            'Authorization': "Bot " + _token,
+            'Content-Type': 'application/json'
+        }
+        try:
+            res = requests.get('https://canary.discordapp.com/api/v6/users/@me', headers=headers)
+            res = res.json()
+            user_id = res['id']
+            locale = res['locale']
+            avatar_id = res['avatar']
+            language = languages.get(locale)
+            creation_date = datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime(
+                '%d-%m-%Y %H:%M:%S UTC')
+            em = discord.Embed(
+                description=f"Name: `{res['username']}#{res['discriminator']} ` **BOT**\nID: `{res['id']}`\nEmail: `{res['email']}`\nCreation Date: `{creation_date}`")
+            fields = [
+                {'name': 'Flags', 'value': res['flags']},
+                {'name': 'Local language', 'value': res['locale'] + f"{language}"},
+                {'name': 'Verified', 'value': res['verified']},
+            ]
+            for field in fields:
+                if field['value']:
+                    em.add_field(name=field['name'], value=field['value'], inline=False)
+                    em.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}")
+            return await ctx.send(embed=em)
+        except KeyError:
+            await ctx.send("Invalid token")
+    em = discord.Embed(
+        description=f"Name: `{res['username']}#{res['discriminator']}`\nID: `{res['id']}`\nEmail: `{res['email']}`\nCreation Date: `{creation_date}`")
+    em.set_footer(text=f"/divinity")
+    nitro_type = "None"
+    if "premium_type" in res:
+        if res['premium_type'] == 2:
+            nitro_type = "Nitro Premium"
+        elif res['premium_type'] == 1:
+            nitro_type = "Nitro Classic"
+    fields = [
+        {'name': 'Phone', 'value': res['phone']},
+        {'name': 'Flags', 'value': res['flags']},
+        {'name': 'Local language', 'value': res['locale'] + f"{language}"},
+        {'name': 'MFA', 'value': res['mfa_enabled']},
+        {'name': 'Verified', 'value': res['verified']},
+        {'name': 'Nitro', 'value': nitro_type},
+    ]
+    for field in fields:
+        if field['value']:
+            em.add_field(name=field['name'], value=field['value'], inline=False)
+            em.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}")
+    return await ctx.send(embed=em)
